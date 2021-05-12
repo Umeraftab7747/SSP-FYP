3bf5b1181b22dd0afe5853c043dc9b9e5d4a7934
Umer Aftab <umeraftab7747@gmail.com>
2021-05-12 19:19:38 +0500

    add

--- src/screens/users/Profile.js
+++ src/screens/users/Profile.js
@@ -1,14 +1,50 @@
 import React, {Component} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
+import {AppInput, Header} from '../../Components';
 
 export class Profile extends Component {
+  state = {
+    Name: '',
+  };
   render() {
     return (
-      <View>
-        <Text> Profile </Text>
+      <View style={styles.Container}>
+        <Header text={'UPDATE PROFILE'} />
+        {/* edit info */}
+        <AppInput
+          IconName={'add'}
+          Placeholder={'Name of the service'}
+          onChangeText={Name => {
+            this.setState({Name});
+          }}
+          value={this.state.Name}
+        />
+        {/* edit info */}
+        <AppInput
+          IconName={'add'}
+          Placeholder={'Name of the service'}
+          onChangeText={Name => {
+            this.setState({Name});
+          }}
+          value={this.state.Name}
+        />
+        {/* edit info */}
+        <AppInput
+          IconName={'add'}
+          Placeholder={'Name of the service'}
+          onChangeText={Name => {
+            this.setState({Name});
+          }}
+          value={this.state.Name}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
